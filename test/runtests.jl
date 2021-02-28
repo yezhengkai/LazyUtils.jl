f61243d12ffe294eaaf3cd4dee878b1f7c466131
kai <supon3060@gmail.com>
2021-02-28 21:52:16 +0800

    Add `get_pkg_version` test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,11 @@ using Pkg
     @testset "PkgUtils" begin
         environment = joinpath(Pkg.envdir(), "v$(VERSION.major).$(VERSION.minor)")
         projectfile = joinpath(environment, "Project.toml")
-        environments = get_environments();
-        projectfiles = get_projectfiles();
-        @test any(occursin.(environment, environments))
-        @test any(occursin.(projectfile, projectfiles))
+        environments = get_environments()
+        projectfiles = get_projectfiles()
+        @test any(occursin.(environment, environments)) || environments === nothing
+        @test any(occursin.(projectfile, projectfiles)) || projectfiles === nothing
+        @test get_pkg_version("Test") === nothing
+        @test get_pkg_version("TOML").major == 1
     end
 end
