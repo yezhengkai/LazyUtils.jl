1f73bed701155ed830c28907d2f707e252aa3c40
Documenter.jl <documenter@juliadocs.github.io>
2022-03-21 13:26:07 +0000

    build based on 74897d8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LazyUtils","category":"page"},{"location":"#LazyUtils","page":"Home","title":"LazyUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LazyUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LazyUtils]","category":"page"}]
+}
